06e27532e601f30f296686282037e0c931263247
Eugene <jenyafocus@gmail.com>
2020-10-25 17:29:30 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Hello");
+        builder.append("there!");
+        System.out.println(builder.toString());
+    }
+}
